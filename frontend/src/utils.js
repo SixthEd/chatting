1c37f3f99b427a7a19cae974f166fb124867012b
SixthEd <rohan.mahor92@gmail.com>
2025-08-01 17:43:15 +0000

    fix canvas ui

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 const axioInst = axios.create({
-    withCredentials:true,
-    baseURL: "http://ec2-13-127-116-35.ap-south-1.compute.amazonaws.com:80/"});
+    withCredentials:true, 
+    baseURL: "http://ec2-13-127-116-35.ap-south-1.compute.amazonaws.com:80/"
+	});
+
+
 export default axioInst;
